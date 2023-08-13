b84bbff406edf116f8665bc0b25ddf7e8cddba0d
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-08-13 13:10:24 +0800

    fixed user and user profile relation

--- posthub/app/Models/UserProfile.php
+++ posthub/app/Models/UserProfile.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserProfile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'birthday',
+        'phone_number',
+        'address_1',
+        'address_2',
+        'city',
+        'zip_code',
+        'province',
+        'country',
+        'profile_picture'
+    ];
+
+    protected $casts = [
+        'birthday' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
