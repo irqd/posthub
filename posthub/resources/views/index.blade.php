2b3d4429f57fabc543fbed10c10222a9ed017f28
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-11-02 14:22:02 +0800

    updated index

--- posthub/resources/views/index.blade.php
+++ posthub/resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
-    <div class="vh-100">
-        Hello World
+    <div class="vh-100 d-flex flex-column justify-content-start align-items-center mt-5">
+        <h1>POST HUB</h1>
+        <small>Put some introductory sentences here if you have some.</small>
     </div>
 </x-layout>
